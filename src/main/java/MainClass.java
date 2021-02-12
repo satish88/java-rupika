cd32dab61c64a8ed9b5d5093b478822328041e2f
Satish Gupta <satish88@gmail.com>
2021-02-11 20:17:53 -0800

    First Commit

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,2 +1,59 @@
 public class MainClass {
+    // args[0] = what type of input is this
+    // args[1]  = what type of output
+    // args[2] = input value
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+
+        if (args[0].equals("mm") && args[1].equals("cm")) {
+            // use mm to cm converter
+            int i = Integer.parseInt(args[2]);
+            float result = i / 10.0f;
+            System.out.println(result);
+        }
+        else if (args[0].equals("cm") && args[1].equals("mm")) {
+            // use mm to cm converter
+            int i = Integer.parseInt(args[2]);
+            float result = i * 10.0f;
+            System.out.println(result);
+        }
+
+
+
+    }
+
+
+
+    // method definition
+    public static void abc() {
+        System.out.println("I am a method");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    // <verb> is a method whose return type is <> and it takes no arguments
+    // or it takes argument 1 of type <>, argument 2 of type<>,.....
+
+
+
+
+
 }
+
+
+
+
+// basic statements
+// what are the variables
+// if else condition
+// loop: while loop, for loop, do while
+// switch case statement
+// methods
+// classes
+
+
+
---
+++ src/main/java/SecondClass.java
@@ -0,0 +1,57 @@
+public class SecondClass {
+
+
+
+    public static void main(String[] args) {
+        // variable define, declare , initialization (give value)
+        int i = 0;
+
+        float r = 3.0f;
+
+        String name = "Yukti";
+
+        MainClass myClass = new MainClass();
+
+        // in built types
+//        int, float, char, String, double
+
+        Integer a = Integer.MAX_VALUE;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(a);
+
+
+
+
+        // <type> <name> = <value>;
+
+        // method define
+        // <access_modifier> <return_type> <method_name>(<arguments>) {
+        // }
+
+        //<access_modifier> <return_type> <method_name>(<argument1>, <argument2>,...) {
+        // }
+
+        // method call
+        // <method_name>(<arguments>);
+
+
+    }
+
+
+
+
+}
+
+
+
+
+// basic statements
+// what are the variables
+// if else condition
+// loop: while loop, for loop, do while
+// switch case statement
+// methods
+// classes
+
+
+
