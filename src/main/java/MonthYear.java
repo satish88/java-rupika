451f68b3d885e71f1c02895910f460bc34b586e6
Satish Gupta <satish88@gmail.com>
2021-02-17 13:38:27 -0800

    online class code commit

--- src/main/java/MonthYear.java
+++ src/main/java/MonthYear.java
@@ -1,5 +1,13 @@
 public class MonthYear {
 
+    // signature of a method
+    // name of the method
+    // access modifier
+    // return type
+    // argument details
+        // name and type of argument
+        // ...
+
     public int calculateDaysInMonth(String month){
         System.out.println("Make sure you provide first letter capital in Month name!");
 
@@ -24,15 +32,15 @@ public class MonthYear {
     }
 
 
-    public int calculateDaysInYear(int yearname){
-        if (yearname % 4 == 0) {
+    public int calculateDaysInYear(int yearName){
+        if (yearName % 4 == 0) {
             int x = 366;
-            System.out.println(yearname +" has 366 Days. ");
+            System.out.println(yearName +" has 366 Days. ");
             return (x);
         }
         else{
             int y = 365;
-            System.out.println(yearname + " has 365 Days. ");
+            System.out.println(yearName + " has 365 Days. ");
             return (y);
         }
     }
