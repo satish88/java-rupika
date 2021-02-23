b1c94872ad572d98c1756297f827153b2350b507
Yukti Aggarwal <yukti.1397@gmail.com>
2021-02-23 21:35:55 +0530

    3 Test Factorial 1 is failed and 2 is passed.

--- src/test/java/YuktiFactorialTest.java
+++ src/test/java/YuktiFactorialTest.java
@@ -11,6 +11,7 @@ public class YuktiFactorialTest {
         Assert.assertEquals(result, "result1 is too large.", "Calculate the 99!");
     }
 
+    // Test 2 is for negative Integer.
     @Test
     public void calculateNegativeIntegerFactorial() {
         Factorial test2 = new Factorial();
@@ -18,7 +19,7 @@ public class YuktiFactorialTest {
         Assert.assertEquals(result, -56, "Provide positive number input!");
     }
 
-
+    // Test 3 is for Zero factorial.
     @Test
     public void calculateZeroFactorial() {
         Factorial test3 = new Factorial();
