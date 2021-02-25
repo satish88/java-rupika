bcc21220da6fcf48bf43ac90b6df0efa0123c344
Rupika Gupta <rupi.1788@gmail.com>
2021-02-25 12:53:25 -0800

    fixing bug for JR-2

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -1,25 +1,25 @@
 public class Factorial {
-    //how to calculate n factorial;
-    public static int calculate(int n){
-        int result = 1;
-        if(n >=1){
-           for( int counter = n; counter>=1; counter= counter -1){
-            result = result * counter;
-           }
+    //how to calculate n factorial where 20>= n>=1;
+    public static Long calculate(int n){
+        Long result = 1l;
+        if(20 >= n && n >=1) {
+            for (int counter = n; counter >= 1; counter = counter - 1) {
+                result = result * counter;
+            }
         }
         else if (n==0) {
-            result = 1;
+            result = 1l;
         }
         else{
             System.out.println("Information not available.");
-            System.out.println("Provide positive number input!");
-            result = n;
+            System.out.println("Provide 1 to 20 number input!");
+            result =(n)*1l;
         }
         return result;
     }
 
     public static void main(String[] args){
-         int result = calculate(3);
+         Long result = calculate(20);
         System.out.println(result);
 
     }
--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -3,22 +3,24 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class FactorialTest {
-@Test
-    public void testForCalculatingFactorialOfPositiveNumbers(){
-    Factorial factorial = new Factorial();
-    int result = factorial.calculate(3);
-    assertEquals(result, 6, "value of 3!");
-    }
     @Test
-    public void testForCalculatingFactorialOfZero(){
+    public void testForCalculatingFactorialOfPositiveNumbers() {
         Factorial factorial = new Factorial();
-        int result = factorial.calculate(0);
-        assertEquals(result, 1, "value of 0!");
+        long result = factorial.calculate(20);
+        assertEquals(result, 2432902008176640000l, "value of 20!");
     }
+
     @Test
-    public void testForCalculatingFactorialOfNegativeNumbers(){
+    public void testForCalculatingFactorialOfZero() {
         Factorial factorial = new Factorial();
-        int result = factorial.calculate(-2);
-        assertEquals(result, -2, "value of -2!");
+        long result = factorial.calculate(0);
+        assertEquals(result, 1l, "value of 0!");
+    }
+
+    @Test
+    public void testForCalculatingFactorialOfNegativeNumbers() {
+        Factorial factorial = new Factorial();
+        long result = factorial.calculate(-2);
+        assertEquals(result, -2l, "value of -2!");
     }
 }
