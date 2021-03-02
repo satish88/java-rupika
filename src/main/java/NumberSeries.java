16b6915264c3f21e06d789b06339d928635306ac
Rupika Gupta <rupi.1788@gmail.com>
2021-03-01 17:06:42 -0800

    sum of 1 to n numbers.

--- src/main/java/NumberSeries.java
+++ src/main/java/NumberSeries.java
@@ -1,5 +1,25 @@
 public class NumberSeries {
 
+    // Find the sum of 1 to n numbers.
+  public static int seriesSum (int n){
+      int result = 0;
+      if (n > 1){
+          for (int counter = 1; counter <= n;  counter++){
+              result = result + counter;
+          }
+      }
+      else if (n < 1) {
+          for (int counter = 1; counter >= n;  counter-- ) {
+              result = result + counter;
+          }
+      }
+      return result;
+  }
+
+
+
+
+
     //if starting no. is 'a' and last no. is 'target';
     // find the sum of all the multiples of 3 and 5 in between;
 
@@ -45,6 +65,9 @@ public class NumberSeries {
 
     public static void main(String[]args){
         int result = sum(1, -2);
+        System.out.println(result);
+
+        result = seriesSum(-3);
         System.out.println(result);
     }
 }
--- src/test/java/NumberSeriesTest.java
+++ src/test/java/NumberSeriesTest.java
@@ -36,4 +36,18 @@ public class NumberSeriesTest {
         int result =test.sum(-6,-6);
         assertEquals(result, -6, "sum of multiples of 3 and 5 from -6 to -6");
     }
+    @Test
+    public void testForAddingPositiveIntegerSeriesFromOne(){
+        NumberSeries test = new NumberSeries();
+        int result =test.seriesSum(4);
+        assertEquals(result, 10, "sum of 1 to 4");
+    }
+    @Test
+    public void testForAddingNegativeIntegerSeriesFromOne(){
+        NumberSeries test = new NumberSeries();
+        int result =test.seriesSum(-4);
+        assertEquals(result, -9, "sum of 1 to -4");
+    }
+
+
 }
