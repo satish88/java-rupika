9f3810a843c1d8a9a4e9a1b350cebec6ff27cc0d
Yukti Aggarwal <yukti.1397@gmail.com>
2021-03-08 21:29:11 +0530

    JR-20

--- src/test/java/MonthYearTest.java
+++ src/test/java/MonthYearTest.java
@@ -1,5 +1,8 @@
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 @Test
@@ -21,6 +24,25 @@ public class MonthYearTest {
         MonthYear month = new MonthYear();
         boolean checkCentury = MonthYear.checkLeapYear(1900);
         assertEquals(checkCentury, false, "1900 is not a leap year.");
+    }
+
+    public void daysInAYear() {
+        MonthYear month = new MonthYear();
+        int day = MonthYear.calculateDaysInYear(1997);
+        assertEquals(day, 365, "1997 has 365 days.");
+    }
+
+    public void daysInACenturyYear() {
+        MonthYear day = new MonthYear();
+        int daysInAYear = MonthYear.calculateDaysInYear(2100);
+        assertEquals(daysInAYear, 365, "Year 2100 will have 356 days.");
+    }
+
+    public void printLeapYears() {
+        MonthYear years = new MonthYear();
+        List<Integer> year = MonthYear.printLeapYears(1895, 3);
+        List<Integer> expectedYears = Arrays.asList(1896, 1904, 1908);
+        assertEquals(year, expectedYears, "print next 3 leap years after 1895.");
     }
 
 }
